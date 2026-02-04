e647ea76982bedfc121d3c10dcba35b313e953c0
Vivek Kumar <vivekkumarjnv054@gmail.com>
2026-02-04 22:11:38 +0530

    chore: update project dependencies by adding various node modules to client and server.

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api", // Adjust if backend port is different
+  baseURL: "http://localhost:3000/api",
 });
 
 // Add interceptor to include token in requests
